2fe85403423da5af6dc23a074e4336c5d5c40c10
tacticalretard0 <47053156+tacticalretard0@users.noreply.github.com>
2021-08-16 19:53:34 -0700

    first commit

---
+++ pylinalg/two.py
@@ -0,0 +1,82 @@
+from math import sqrt, radians, sin, cos
+
+class Vec2:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    ####
+    # RETURNS
+
+    def length(self):
+        return sqrt(self.x*self.x + self.y*self.y)
+
+    def get_normalized(self):
+        le = self.length()
+
+        if le != 0:
+            return Vec2(self.x / le, self.y / le)
+        
+        return Vec2()
+
+    def dot(self, other):
+        return self.x*other.x + self.y*other.y
+
+    def get_rounded(self, places=0):
+        return Vec2(round(self.x, places), round(self.y, places))
+
+    def get_truncated(self):
+        return Vec2(int(self.x), int(self.y))
+
+    def get_rotated(self, deg):
+        theta = radians(deg)
+
+        st = sin(theta)
+        ct = cos(theta)
+
+        newx = self.y * st + self.x * ct
+        newy = self.y * ct - self.x * st
+
+        return Vec2(newx, newy)
+
+    def get_rotatedc(self, deg):
+        theta = radians(deg)
+
+        st = sin(theta)
+        ct = cos(theta)
+
+        newx = self.x * ct - y * st
+        newy = self.x * st + y * ct
+
+        return Vec2(newx, newy)
+
+    def copy(self):
+        return Vec2(self.x+0, self.y+0)
+
+    ####
+    # MODIFIES SELF
+        # none yet
+
+    ####
+    # OPERATOR OVERLOADING
+
+    def __repr__(self):
+        return f"Vec2({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        if isinstance(other, Vec2):
+            return Vec2(self.x + other.x, self.y + other.y)
+
+        if isinstance(other, (int, float)):
+            return Vec2(self.x + other, self.y + other)
+
+    def __sub__(self, other):
+        if isinstance(other, Vec2):
+            return Vec2(self.x - other.x, self.y - other.y)
+
+        if isinstance(other, (int, float)):
+            return Vec2(self.x - other, self.y - other)
+
---
+++ test.py
@@ -0,0 +1,51 @@
+import sys
+
+from math import pi, e
+
+import pylinalg.two as pyla2
+
+
+correct = {
+    "length": 10,
+    "get_normalized": pyla2.Vec2(1, 0),
+    "get_rounded": pyla2.Vec2(3, 3),
+    "get_truncated": pyla2.Vec2(3, 2),
+    "get_rotated": pyla2.Vec2(0, -10),
+}
+
+def info(passed, res, action):
+    print("="*5)
+    print(action, end=", ")
+    print("test passed" if passed else "!! test failed !!")
+    print()
+    print(f"result: {res}")
+    print()
+    print(f"expected: {correct[action]}")
+    print("-"*5)
+    print()
+
+def check(res, action):
+    if res != correct[action]:
+        info(False, res, action)
+        sys.exit()
+
+    info(True, res, action)
+
+testvec = pyla2.Vec2(10, 0)
+
+check(testvec.length(), "length")
+
+check(testvec.get_normalized(), "get_normalized")
+
+get_rounded_test = pyla2.Vec2(pi, e)
+check(get_rounded_test.get_rounded(), "get_rounded")
+del get_rounded_test
+
+get_truncated_test = pyla2.Vec2(pi, e)
+check(get_truncated_test.get_truncated(), "get_truncated")
+del get_truncated_test
+
+get_rotated_test = testvec.get_rotated(90).get_rounded()
+check(get_rotated_test, "get_rotated")
+del get_rotated_test
+
